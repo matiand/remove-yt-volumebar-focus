26e960aca43aad6e0c1991041f8d8c23beff11ed
itsresool <15248621+itsresool@users.noreply.github.com>
2022-01-02 15:53:51 +0100

    Ensure video is loaded before applying our fix

--- remove-yt-volumebar-focus.js
+++ remove-yt-volumebar-focus.js
@@ -2,15 +2,20 @@
 // @name     Remove yt volumebar focus
 // @description Sets focus to video element after using volume bar, so that left/right arrow keys can be used for video seeking instead of volume change
 // @license MIT
-// @version  1
+// @version  1.2
 // @grant    none
 // @match    https://www.youtube.com/*
 // @namespace https://greasyfork.org/users/847699
 // ==/UserScript==
 
+let videoElem = document.querySelector("video.html5-main-video");
+while(videoElem == null){
+	videoElem = document.querySelector("video.html5-main-video");
+  setInterval(1000)
+}
+
 const volumeArea = document.querySelector(".ytp-volume-area");
 const videoProgressBar = document.querySelector(".ytp-progress-bar-container");
-const videoElem = document.querySelector("video.html5-main-video");
 
 volumeArea.addEventListener("click", (_) => {
     setTimeout(() => {
