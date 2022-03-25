b179353b1f611495fc914596045c2237246738b1
itsresool <15248621+itsresool@users.noreply.github.com>
2022-03-25 17:39:31 +0100

    Use mutation observer for applying our fix
    Previous solution didn't work when navigating from search or channel
    page.

--- remove-yt-volumebar-focus.js
+++ remove-yt-volumebar-focus.js
@@ -2,29 +2,37 @@
 // @name     Remove yt volumebar focus
 // @description Sets focus to video element after using volume bar, so that left/right arrow keys can be used for video seeking instead of volume change
 // @license MIT
-// @version  1.2
+// @version  1.3
 // @grant    none
 // @match    https://www.youtube.com/*
 // @namespace https://greasyfork.org/users/847699
 // ==/UserScript==
 
-let videoElem = document.querySelector("video.html5-main-video");
-while(videoElem == null){
-	videoElem = document.querySelector("video.html5-main-video");
-  setInterval(1000)
-}
+const getVideoElem = () => document.querySelector("video.html5-main-video");
+const elementToObserve = document.querySelector("body");
 
-const volumeArea = document.querySelector(".ytp-volume-area");
-const videoProgressBar = document.querySelector(".ytp-progress-bar-container");
+const observer = new MutationObserver(function(mutations_list) {
+	mutations_list.forEach(function(mutation) {
+		mutation.addedNodes.forEach(function(added_node) {
+			if(added_node.id == 'movie_player') {
+                const volumeArea = document.querySelector(".ytp-volume-area");
+                const videoProgressBar = document.querySelector(".ytp-progress-bar-container");
 
-volumeArea.addEventListener("click", (_) => {
-    setTimeout(() => {
-        videoElem.focus();
-    }, 50);
+                volumeArea.addEventListener("click", (_) => {
+                    setTimeout(() => {
+                        getVideoElem().focus();
+                    }, 50);
+                });
+                videoProgressBar.addEventListener("click", (_) => {
+                    setTimeout(() => {
+                        getVideoElem().focus();
+                    }, 50);
+                });
+
+				observer.disconnect();
+			}
+		});
+	});
 });
 
-videoProgressBar.addEventListener("click", (_) => {
-    setTimeout(() => {
-        videoElem.focus();
-    }, 50);
-});
+observer.observe(elementToObserve, {subtree: true, childList: true});
